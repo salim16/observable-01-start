66367bb11cd83a59cf0f76d80fff2f2072b08642
Mohd Salim <mohd.salim@soprasteria.com>
2019-08-02 14:58:49 +0530

    First Commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  id: number;
+
+  constructor(private route: ActivatedRoute) {
+  }
+
+  // When Routing is used, component is loaded only when there is change in the route. 
+  // or change in URL for that matter. Corresponding component is loaded when URL changes.
+  // if the URL has some dynamic parameter, or path param like in this case, then user component won't get 
+  // loaded again and again.
+
+  ngOnInit() {
+    console.log("User Component is loaded ... ");
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params.id;
+    });
+  }
+}
